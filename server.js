88527a0db0e02dc584bc0b2bb2ee14617f45a9b5
Leticia Caroline Leonardo <leticiacaroline-123@hotmail.com>
2020-02-18 15:55:41 -0300

    Trabalhando com os dados do formulário

--- server.js
+++ server.js
@@ -7,16 +7,58 @@ const server = express()
 server.use(express.static('public'))
 
 
+// habilitar body do formulário
+server.use(express.urlencoded({extended: true}))
+
+
 //congfigurando  a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
-    express: server
+    express: server,
+    noCache: true, //bolean ou booleano aceita dois valores, verdadeiro ou falso
 })
+
+
+// lista de doadores: vetor ou array
+const donors = [
+    {
+        name: "Diego Fernandes",
+        blood: "AB+"
+    },
+    {
+        name: "Cleiton Souza",
+        blood: "B+"
+    },
+    {
+        name: "Robson Marques",
+        blood: "O+"
+    },
+    {
+        name: "Mayk Brito",
+        blood: "A-"
+    }
+]
 
 
 // configurar apresentação da página
 server.get("/", function(req, res) {
-    return res.render("index.html")
+    return res.render("index.html", { donors })
+})
+
+
+server.post("/", function(req, res) {
+    //pegar dados do formulario.
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    // coloco valores dentro do array
+    donors.push({
+        name: name,
+        blood: blood,
+    })
+
+    return res.redirect("/")
 })
 
 
